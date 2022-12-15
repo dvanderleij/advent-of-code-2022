9b48ecae95fa7e2e721037cd43481514240a1c57
David <davidvanderleij@gmail.com>
2022-12-15 15:25:44 +0100

    9

---
+++ src/main/kotlin/nine/Main.kt
@@ -0,0 +1,19 @@
+package nine
+
+import java.lang.IllegalArgumentException
+import kotlin.io.path.readText
+
+const val INPUT_FILE = "nine.txt"
+fun main() {
+    one()
+    two()
+}
+
+fun one() {
+
+}
+
+fun two() {
+}
+
+
